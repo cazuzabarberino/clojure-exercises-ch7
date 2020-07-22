c9e89a6360f4a1339d34fd8853304c33f0317c53
cazuzaneves <cazuzaneves@gmail.com>
2020-07-22 18:13:17 -0300

    end

--- src/clojure_exercices_ch7/core.clj
+++ src/clojure_exercices_ch7/core.clj
@@ -5,3 +5,32 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(eval (read-string "(list \"Cazuza\" \"Star Wars\")"))
+
+(defn infix
+  [expr]
+  (let [expr (loop [new-list () p-v nil [val & remaining-vals] expr]
+               (if (nil? val)
+                 new-list
+                 (if (or (= (str val) "*") (= (str val) "/"))
+                   (recur (concat (butlast new-list)
+                                  (list (list val p-v (first remaining-vals))))
+                          (list val p-v (first remaining-vals)) (rest remaining-vals))
+                   (if (list? val)
+                     (let [recursion-result (infix val)]
+                       (recur (concat new-list (list recursion-result)) recursion-result remaining-vals))
+                     (recur (concat new-list (list val)) val remaining-vals)))))]
+    (loop [new-expr (first expr) [val & remaining-vals] expr]
+      (if (nil? val)
+        new-expr
+        (recur (if (or (= (str val) "+") (= (str val) "-"))
+                 (list val new-expr (first remaining-vals))
+                 new-expr)
+               remaining-vals)))))
+
+(defmacro infix-macro
+  [expr]
+  (infix expr))
+
+(infix-macro ((1 + 3) * 4 / 3))
